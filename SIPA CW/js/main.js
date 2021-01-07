3f906dced80575611acda433dc8c1a983c12fdf5
Anjali Chauhan <anjc24@gmail.com>
2021-01-08 02:51:46 +0530

    landing page changes

--- SIPA CW/js/main.js
+++ SIPA CW/js/main.js
@@ -10,6 +10,20 @@
     }
   });
 
+
+  $('.counter-count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+      
+      //chnage count up speed here
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
   // Back to top button
   $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
