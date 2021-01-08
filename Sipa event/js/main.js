6cb5e644d41fdf6b9adce48cd0ecf2ddd2c17240
Anjali Chauhan <anjc24@gmail.com>
2021-01-08 14:47:21 +0530

    event website changes

--- Sipa event/js/main.js
+++ Sipa event/js/main.js
@@ -4,6 +4,22 @@
 	Version: 1.0
 --------------------------------------
 ======================================*/
+let navbar = $(".header-section");
+
+$(window).scroll(function () {
+  // get the complete hight of window
+  let oTop = $(".header-section").offset().top - window.innerHeight;
+  if ($(window).scrollTop() > 100) {
+    navbar.addClass("sticky");
+    $(".nav-link").hover(function(){
+      $(this).css("color", "#616161");
+    }, function(){
+      $(this).css("color", "#fff");
+    });
+  }else {
+    navbar.removeClass("sticky");
+  }
+});
 
 
 'use strict';
